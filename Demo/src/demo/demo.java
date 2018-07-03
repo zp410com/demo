3b5538e36b6b7ed68286d8acc738d0c1d1e2b58f
zp410com <15190471594@163.com>
2018-07-03 23:55:10 +0800

    commit selenium1.java

--- Demo/src/demo/demo.java
+++ Demo/src/demo/demo.java
@@ -6,7 +6,7 @@ public class demo {
 
 public static void main(String[] args) {
         WebDriver driver;
-       String url =" https://github.com/zp410com/demo.git";
+       String url ="http://www.baidu.com";
         driver = new ChromeDriver();                                                                               
 
         driver.get(url);   
---
+++ Demo/src/demo/selenium1.java
@@ -0,0 +1,14 @@
+package demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class selenium1 {
+
+	public static void main(String[] args) {
+		  WebDriver driver;
+	       String url ="http://www.baidu.com";
+	        driver = new ChromeDriver(); 
+	}
+
+}
