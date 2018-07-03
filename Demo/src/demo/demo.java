10955639f1b9432bc01d3190c532938e5929feaf
zp410com <15190471594@163.com>
2018-07-03 23:26:23 +0800

    commit Demo project

---
+++ Demo/src/demo/demo.java
@@ -0,0 +1,19 @@
+package demo;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class demo {
+
+public static void main(String[] args) {
+        WebDriver driver;
+        driver = new ChromeDriver();                                                                               
+
+        driver.get("https://www.baidu.com/");   
+
+     
+
+}
+
+
+
+}
