56b7e01ea0a34d4a3aab4ad835dd99d22e2ebe7c
zp410com <15190471594@163.com>
2018-07-04 12:45:37 +0800

    selenium1.java change

--- Demo/src/demo/selenium1.java
+++ Demo/src/demo/selenium1.java
@@ -1,5 +1,6 @@
 package demo;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -7,8 +8,21 @@ public class selenium1 {
 
 	public static void main(String[] args) {
 		  WebDriver driver;
-	       String url ="http://www.baidu.com";
+	       String url ="https://www.imooc.com/user/newlogin/from_url";
 	        driver = new ChromeDriver(); 
+	        driver.get(url);
+	        driver.manage().window().maximize();   //窗口最大化
+
+	        driver.findElement(By.id("auto-signin")).click();
+	        driver.findElement(By.linkText("立即注册")).click();
+	        driver.findElement(By.partialLinkText("直接")).click();  //partialLinkText获取部分字段
+	        driver.findElement(By.tagName("input")).sendKeys("18844996590");
+	        driver.findElement(By.name("password")).sendKeys("19951108ZXZzxz");
+	        driver.findElement(By.className("moco-btn-lg")).click();  //登陆
+	        
+//	        driver.manage().window().maximize();   //窗口最大化
+	        
+	        
 	}
 
 }
