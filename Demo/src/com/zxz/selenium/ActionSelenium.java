2d211392f815afe3d0220ea267db57cc4a643d14
zp410com <15190471594@163.com>
2018-07-09 22:34:59 +0800

    com.zxz.selenium commit

---
+++ Demo/src/com/zxz/selenium/ActionSelenium.java
@@ -0,0 +1,9 @@
+package com.zxz.selenium;
+
+public class ActionSelenium {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
--- Demo/src/demo/selenium1.java
+++ Demo/src/demo/selenium1.java
@@ -1,25 +1,36 @@
 package demo;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class selenium1 {
 
 	public static void main(String[] args) {
 		  WebDriver driver;
-	       String url ="https://www.imooc.com/user/newlogin/from_url";
+//	       String url1 ="https://www.imooc.com/user/newlogin/from_url";
+	       String url2 = "https://www.imooc.com/";
 	        driver = new ChromeDriver(); 
-	        driver.get(url);
-	        driver.manage().window().maximize();   //窗口最大化
+	        driver.get(url2);
+//	        driver.manage().window().maximize();   //窗口最大化
 
-	        driver.findElement(By.id("auto-signin")).click();
-	        driver.findElement(By.linkText("立即注册")).click();
-	        driver.findElement(By.partialLinkText("直接")).click();  //partialLinkText获取部分字段
-	        driver.findElement(By.tagName("input")).sendKeys("18844996590");
-	        driver.findElement(By.name("password")).sendKeys("19951108ZXZzxz");
-	        driver.findElement(By.className("moco-btn-lg")).click();  //登陆
+//	        driver.findElement(By.id("auto-signin")).click();
+//	        driver.findElement(By.linkText("立即注册")).click();
+//	        driver.findElement(By.partialLinkText("直接")).click();  //partialLinkText获取部分字段
+//	        driver.findElement(By.tagName("input")).sendKeys("18844996590");
+//	        driver.findElement(By.name("password")).sendKeys("19951108ZXZzxz");
+//	        driver.findElement(By.className("moco-btn-lg")).click();  //登陆
 	        
+	        
+	        driver.findElement(By.xpath("//*[@id=\"nav\"]/div[3]/div[2]/input[1]")).sendKeys("java");
+	        
+	        driver.findElement(By.xpath("//*[@id=\"nav\"]/div[3]/div[3]")).click();
+	        WebElement element = driver.findElement(By.className("nav-item"));
+	        List <WebElement> elements = element.findElements(By.tagName("li"));
+	        elements.get(1).click();
 //	        driver.manage().window().maximize();   //窗口最大化
 	        
 	        
