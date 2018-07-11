2de7365df74ce43615d1c589a86e43f8b23efae1
zp410com <15190471594@163.com>
2018-07-12 00:47:12 +0800

    commit selenium1.java

--- Demo/src/demo/selenium1.java
+++ Demo/src/demo/selenium1.java
@@ -6,31 +6,41 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class selenium1 {
+	WebDriver driver;
+
+	public void init() {
+		driver = new ChromeDriver();
+		String url1 = "https://www.imooc.com/user/newlogin/from_url";
+		driver.get(url1);
+	}
+
+	// 单选框
+	public void initLogin() {
+		driver.findElement(By.tagName("input")).sendKeys("18844996590");
+		driver.findElement(By.name("password")).sendKeys("19951108ZXZzxz");
+		driver.findElement(By.className("moco-btn-lg")).click(); // 登陆
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void radioBox() {
+		driver.get("https://www.imooc.com/user/setprofile");
+		driver.findElement(By.xpath("//*[@id=\"setting-profile\"]/div[2]/a")).click();
+	}
 
 	public static void main(String[] args) {
-		WebDriver driver;
-//	       String url1 ="https://www.imooc.com/user/newlogin/from_url";
-		String url2 = "https://www.imooc.com/";
-		driver = new ChromeDriver();
-		driver.get(url2);
-//	        driver.manage().window().maximize();   //窗口最大化
 
-//	        driver.findElement(By.id("auto-signin")).click();
-//	        driver.findElement(By.linkText("立即注册")).click();
-//	        driver.findElement(By.partialLinkText("直接")).click();  //partialLinkText获取部分字段
-//	        driver.findElement(By.tagName("input")).sendKeys("18844996590");
-//	        driver.findElement(By.name("password")).sendKeys("19951108ZXZzxz");
-//	        driver.findElement(By.className("moco-btn-lg")).click();  //登陆
-
-		driver.findElement(By.xpath("//*[@id=\"nav\"]/div[3]/div[2]/input[1]")).sendKeys("java");
-
-		driver.findElement(By.xpath("//*[@id=\"nav\"]/div[3]/div[3]")).click();
-		WebElement element = driver.findElement(By.className("nav-item"));
-		List<WebElement> elements = element.findElements(By.tagName("li"));
-		elements.get(1).click();
-//	        driver.manage().window().maximize();   //窗口最大化
+		selenium1 sele = new selenium1();
+		sele.init();
+		sele.initLogin();
+		sele.radioBox();
 
 	}
 
