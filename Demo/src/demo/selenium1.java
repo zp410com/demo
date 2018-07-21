ac7c9859ffd72dab6e9942e6272841031975e1a8
zp410com <15190471594@163.com>
2018-07-21 21:25:41 +0800

    下拉框处理

--- Demo/src/demo/selenium1.java
+++ Demo/src/demo/selenium1.java
@@ -7,98 +7,117 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 
 public class selenium1 {
-	WebDriver driver;
+    WebDriver driver;
 
-	public void init() {
-		driver = new ChromeDriver();
+    public void init() {
+        driver = new ChromeDriver();
 //		String url1 = "https://www.imooc.com/user/newlogin/from_url";
-		String url2 = "https://www.imooc.com/";
-		driver.get(url2);
-	}
+        String url2 = "https://www.imooc.com/";
+        driver.get(url2);
+    }
 
-	// 等待
-	public void threadSleep(int time) {
-		try {
-			Thread.sleep(time);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
+    // 等待
+    public void threadSleep(int time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
-	// 登陆
-	public void initLogin() {
-		driver.findElement(By.id("js-signin-btn")).click();
-		threadSleep(2000);
-		driver.findElement(By.xpath("//*[@id=\"signup-form\"]/div[1]/input")).sendKeys("18844996590");
-		driver.findElement(By.xpath("//*[@id=\"signup-form\"]/div[2]/input")).sendKeys("19951108ZXZzxz");
-		driver.findElement(By.xpath("//*[@id=\"signup-form\"]/div[5]/input")).click();
-		threadSleep(3000); //source tree
+    // 登陆
+    public void initLogin() {
+        driver.findElement(By.id("js-signin-btn")).click();
+        threadSleep(2000);
+        driver.findElement(By.xpath("//*[@id=\"signup-form\"]/div[1]/input")).sendKeys("18844996590");
+        driver.findElement(By.xpath("//*[@id=\"signup-form\"]/div[2]/input")).sendKeys("19951108ZXZzxz");
+        driver.findElement(By.xpath("//*[@id=\"signup-form\"]/div[5]/input")).click();
+        threadSleep(3000); //source tree
 
-	}
+    }
 
-	// 单选框
-	public void radioBox() {
-		driver.get("https://www.imooc.com/user/setprofile");
-		driver.findElement(By.xpath("//*[@id=\"setting-profile\"]/div[2]/a")).click();
-		List<WebElement> elements = driver.findElements(By.xpath("//*[@id=\"profile\"]/div[4]/div/label//input"));
+    // 单选框
+    public void radioBox() {
+        driver.get("https://www.imooc.com/user/setprofile");
+        driver.findElement(By.xpath("//*[@id=\"setting-profile\"]/div[2]/a")).click();
+        List<WebElement> elements = driver.findElements(By.xpath("//*[@id=\"profile\"]/div[4]/div/label//input"));
 
-		for (WebElement radio : elements) {
-			boolean flag = radio.isSelected();
-			if (flag == false) {
-				radio.click();
+        for (WebElement radio : elements) {
+            boolean flag = radio.isSelected();
+            if (flag == false) {
+                radio.click();
 //				System.out.println("");
-				break;
-			}
-			System.out.println("nnnnn");
-		}
-	}
+                break;
+            }
+            System.out.println("nnnnn");
+        }
+    }
 
-	// 多选框
+    // 多选框
 
-	public void checkBox() {
-		WebElement check = driver.findElement(By.id("auto-signin"));
-		boolean isSe = check.isSelected();
-		check.click();
-	}
+    public void checkBox() {
+        WebElement check = driver.findElement(By.id("auto-signin"));
+        boolean isSe = check.isSelected();
+        check.click();
+    }
 
-	// 表单
-	public void webForm() {
+    // 表单
+    public void webForm() {
 //		driver.findElement(By.id("id=\"signup-form\"")).submit();
 
-	}
+    }
 
-	// 上传文件
-	public void upFile() {
-		driver.get("https://www.imooc.com/user/setbindsns");
-		threadSleep(5000);
-		/**
-		 * 执行js脚本 80-82
-		 */
-		String jsString = "document.getElementsByClassName('update-avator')[0].style.bottom='0'; ";
-		JavascriptExecutor js = (JavascriptExecutor) driver;
-		js.executeScript(jsString); 
-		threadSleep(1000);	
-		driver.findElement(By.xpath("//*[@id=\"main\"]/div/div[1]/div[1]/div[1]/div/p/a")).click();
-		threadSleep(100);
+    // 上传文件
+    public void upFile() {
+        driver.get("https://www.imooc.com/user/setbindsns");
+        threadSleep(5000);
+        /**
+         * 执行js脚本 80-82
+         */
+        String jsString = "document.getElementsByClassName('update-avator')[0].style.bottom='0'; ";
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript(jsString);
+        threadSleep(1000);
+        driver.findElement(By.xpath("//*[@id=\"main\"]/div/div[1]/div[1]/div[1]/div/p/a")).click();
+        threadSleep(100);
 //		driver.findElement(By.xpath("/html/body/div[7]/div/div[2]/div/div/div[1]/div[2]/form/a")).click();
-		driver.findElement(By.id("upload")).sendKeys("/Users/zhangxianzhao/Desktop/timg.jpeg");   //选择上传路径
-		driver.findElement(By.xpath("/html/body/div[7]/div/div[2]/div/div/div[2]/div/a[1]")).click();
-		
-	}
+        driver.findElement(By.id("upload")).sendKeys("/Users/zhangxianzhao/Desktop/timg.jpeg");   //选择上传路径
+        driver.findElement(By.xpath("/html/body/div[7]/div/div[2]/div/div/div[2]/div/a[1]")).click();
 
-	public static void main(String[] args) {
+    }
 
-		selenium1 sele = new selenium1();
-		sele.init();
+
+    //下拉框操作
+    public void selectBox() {
+        driver.get("https://www.imooc.com/user/setprofile");
+        driver.findElement(By.className("pull-right")).click();
+        threadSleep(1000);
+
+        WebElement element = driver.findElement(By.id("profile")).findElement(By.id("job"));    //层级定位
+        Select downList = new Select(element);
+
+//        downList.selectByIndex(2);    //通过索引
+//        downList.selectByValue("1");    //通过value
+        downList.selectByVisibleText("Web前端工程师");   //通过可见文本
+
+    }
+
+    public static void main(String[] args) {
+
+        selenium1 sele = new selenium1();
+        sele.init();
+
 //		sele.checkBox();
 
-		sele.initLogin();
-		sele.upFile();
+        sele.initLogin();
+        sele.selectBox();
+//        sele.upFile();
 //		sele.radioBox();
 
-	}
+    }
 
 }
