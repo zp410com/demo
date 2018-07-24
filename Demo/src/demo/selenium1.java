4ee84da8c6036467a48a7f420d77143588330a11
zp410com <15190471594@163.com>
2018-07-24 23:26:16 +0800

    iframe切换，和Windows切换操作

--- Demo/src/demo/selenium1.java
+++ Demo/src/demo/selenium1.java
@@ -1,6 +1,7 @@
 package demo;
 
 import java.util.List;
+import java.util.Set;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -14,6 +15,7 @@ import org.openqa.selenium.support.ui.Select;
 
 public class selenium1 {
     WebDriver driver;
+    String windowshandles;
 
     public void init() {
         driver = new ChromeDriver();
@@ -121,11 +123,11 @@ public class selenium1 {
     }
 
     /**
-     *鼠标事件
+     * 鼠标事件
      */
 
-    public void mouseAction(){
-        WebElement element  = driver.findElement(By.xpath("//*[@id=\"main\"]/div[2]/div/div[9]/div[1]/a"));
+    public void mouseAction() {
+        WebElement element = driver.findElement(By.xpath("//*[@id=\"main\"]/div[2]/div/div[9]/div[1]/a"));
 
         Actions actions = new Actions(driver);
 //        actions.click(element).perform();   //模拟鼠标点击
@@ -133,10 +135,48 @@ public class selenium1 {
 //        actions.doubleClick(element).perform(); //双击
 //        threadSleep(1000);
 //        actions.contextClick(element).perform();  //右击
-
+        windowshandles = driver.getWindowHandle();  //获取当前窗口
         actions.moveToElement(element).perform();   //鼠标悬停
-        driver.findElement(By.linkText("区块链")).click(); 
 
+
+        //点击"区块链"
+        driver.findElement(By.xpath("//*[@id=\"main\"]/div[2]/div/div[2]/div[1]/div/div/div/div[2]/a[2]")).click();
+
+
+    }
+
+
+    /**
+     * iframe切换
+     */
+
+    public void iframe(String content) {
+        driver.get("https://www.imooc.com/wiki/create");
+        WebElement iframeElement = driver.findElement(By.id("ueditor_0"));
+        driver.switchTo().frame(iframeElement); //切换到当前iframe
+
+        driver.findElement(By.xpath("/html/body")).sendKeys(content);   //输入内容
+    }
+
+    /**
+     * 窗口切换
+     */
+
+    public void windowsHandle(){
+        //获取到所有的窗口
+        Set<String> handles =driver.getWindowHandles();
+        for (String s: handles) {
+
+            if (s.equals(windowshandles)){
+                //等于当前窗口继续循环，否则跳出循环(等同于else)
+//                continue;
+            }else{
+                driver.switchTo().window(s);
+            }
+
+        }
+        //点击"中级"
+        driver.findElement(By.xpath("//*[@id=\"main\"]/div[1]/div/div[2]/div/div[3]/div/ul/li[4]/a")).click();
 
     }
 
@@ -145,11 +185,13 @@ public class selenium1 {
         selenium1 sele = new selenium1();
         sele.init();
 
-        sele.mouseAction();
 
 //		  sele.checkBox();
 
 //        sele.initLogin();
+//        sele.iframe("测试文本");
+        sele.mouseAction();
+        sele.windowsHandle();
 //        sele.selectBox();
 //        sele.upFile();
 //		  sele.radioBox();
